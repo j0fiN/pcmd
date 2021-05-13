5e8650746ca5a3a787545f0ce96efc9a1fad2905
Jofin <jofinfab@gmail.com>
2021-05-13 14:33:34 +0530

    :white_check_mark: Added tests

--- tests/test_pcmd.py
+++ tests/test_pcmd.py
@@ -1,5 +1,19 @@
 from pcmd import __version__
-
+from pcmd.main import get_commands
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_get_commands_1():
+    assert type(get_commands()).__name__ == "dict"
+
+def test_get_commands_2():
+    assert get_commands()['test'] == "dir"
+
+def test_get_commands_3():
+    try:
+        a = get_commands()['test-3']
+    except:
+        a = None
+    assert a == None
+
