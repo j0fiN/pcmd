a66b212467c4ce6661d8ffc2ebe9a48039d7bf57
Jofin <jofinfab@gmail.com>
2021-06-02 11:41:14 +0530

    :hammer: Added Script which contains all constants.

---
+++ pcmd/__const__.py
@@ -0,0 +1,24 @@
+# contains some constants
+
+EGG = """
+|\\   \\\\\\\\__     o
+| \\_/    o \\    o
+ > _   (( <_  oo
+| / \\__+___/
+|/     |/
+                             _
+ _ __    ___  _ __ ___    __| |
+| '_ \\  / __|| '_ ` _ \\  / _` |
+| |_) || (__ | | | | | || (_| |
+| .__/  \\___||_| |_| |_| \\__,_|
+|_|
+
+"""
+
+
+URLS = {
+    'github': 'https://github.com/j0fiN/pcmd',
+    'pypi': 'https://pypi.org/project/pcmd/',
+    'docs': 'https://j0fin.github.io/pcmd/',
+    'author': 'https://jofin-f-archbald.herokuapp.com/'
+}
