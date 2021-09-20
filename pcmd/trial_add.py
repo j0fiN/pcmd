12400377469bc9edb2748606de82da7f69bbbe3c
j0fiN <jofinfab@gmail.com>
2021-09-20 23:10:38 +0530

    :hammer: tryouts of a new feature for add command.

---
+++ pcmd/trial_add.py
@@ -0,0 +1,37 @@
+"""
+    trial_add
+    ~~~~~~~
+    Tryout of a new feature of add command
+
+    FUNCTIONS
+    ~~~~~~~
+    run
+    main
+"""
+from typing import List
+
+
+def run(command: str, args:List[str]) -> None:
+    '''
+    Adds the arguments wherever $ symbol is found
+    '''
+    # print(command)
+    for arg in args:
+        command = command.replace('$', arg, 1)
+    if command.count('$') > 0:
+        print('All the $ are not filled | ERROR')
+    else:
+        print(command, '| RUNNING')
+
+
+def main():
+    cmd = "cd run $ swallow $"
+    args = ('fall',)
+    run(cmd, args)
+
+    args = ('fall', 'winter')
+    run(cmd, args)
+
+
+if __name__ == "__main__":
+    main()
