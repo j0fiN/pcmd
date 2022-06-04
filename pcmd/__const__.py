31e42ca0dd43b3ac9a28fece4a7e55d2f2e0788f
Jofin F Archbald <jofinfab@gmail.com>
2022-06-04 13:17:58 +0530

    :pencil2: :heavy_plus_sign: Updated link and added colorama

--- pcmd/__const__.py
+++ pcmd/__const__.py
@@ -32,7 +32,7 @@ URLS = {
     'github': 'https://github.com/j0fiN/pcmd',
     'pypi': 'https://pypi.org/project/pcmd/',
     'docs': 'https://j0fin.github.io/pcmd/',
-    'author': 'https://jofin-f-archbald.herokuapp.com/'
+    'author': 'https://jofin.herokuapp.com/'
 }
 
 
